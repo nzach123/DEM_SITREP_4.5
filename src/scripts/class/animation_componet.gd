fd116d8b015d226c894bc1253c1e53dd75ffc74f
nzach123 <nzach123@live.com>
2025-12-18 11:25:39 -0700

    Add AnimationComponent for button hover effects
    Introduced AnimationComponent script to enable hover scaling animations for UI buttons. Updated CourseButton and quiz_scene to use the new component, and adjusted import settings for various assets to improve consistency.

---
+++ src/scripts/class/animation_componet.gd
@@ -0,0 +1,36 @@
+class_name AnimationComponent extends Node
+
+@export var from_center : bool = true
+@export var hover_scale : Vector2 = Vector2(1,1)
+@export var time : float = 0.1
+@export var transition_type : Tween.TransitionType = Tween.TRANS_SINE
+
+var target : Control
+var default_scale : Vector2
+
+func _ready() -> void:
+	target = get_parent()
+	
+	connect_signals()
+	call_deferred("setup")
+	
+func connect_signals() -> void:
+	target.mouse_entered.connect(on_hover)
+	target.mouse_exited.connect(off_hover)
+	
+func setup() -> void:
+	if from_center:
+		target.pivot_offset = target.size /2
+	default_scale = target.scale
+
+
+func on_hover() -> void:
+	add_tween("scale", hover_scale, time)
+	
+func  off_hover() -> void:
+	add_tween("scale", hover_scale, time)
+
+	
+func add_tween(property: String, value, seconds: float) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(target, property, value, seconds).set_trans(transition_type)
