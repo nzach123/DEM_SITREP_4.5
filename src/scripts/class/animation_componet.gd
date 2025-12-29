a831e1b71fb23fd5e3a7871397b5d8a9209b360e
nzach123 <nzach123@live.com>
2025-12-28 18:31:13 -0700

    feat(polish): Add hover/click SFX and visual pulsing to all menu elements

--- src/scripts/class/animation_componet.gd
+++ src/scripts/class/animation_componet.gd
@@ -5,11 +5,20 @@ class_name AnimationComponent extends Node
 @export var time : float = 0.1
 @export var transition_type : Tween.TransitionType = Tween.TRANS_SINE
 
+@export_group("Audio")
+@export var hover_sfx : AudioStream
+@export var click_sfx : AudioStream
+
 var target : Control
 var default_scale : Vector2
+var _audio_player : AudioStreamPlayer
 
 func _ready() -> void:
 	target = get_parent()
+	
+	_audio_player = AudioStreamPlayer.new()
+	_audio_player.bus = &"SFX"
+	add_child(_audio_player)
 	
 	connect_signals()
 	call_deferred("setup")
@@ -17,7 +26,9 @@ func _ready() -> void:
 func connect_signals() -> void:
 	target.mouse_entered.connect(on_hover)
 	target.mouse_exited.connect(off_hover)
-	
+	if target is Button:
+		target.pressed.connect(on_pressed)
+
 func setup() -> void:
 	if from_center:
 		target.pivot_offset = target.size /2
@@ -26,10 +37,17 @@ func setup() -> void:
 
 func on_hover() -> void:
 	add_tween("scale", hover_scale, time)
+	if hover_sfx:
+		_audio_player.stream = hover_sfx
+		_audio_player.play()
 	
 func  off_hover() -> void:
 	add_tween("scale", default_scale, time)
 
+func on_pressed() -> void:
+	if click_sfx:
+		_audio_player.stream = click_sfx
+		_audio_player.play()
 	
 func add_tween(property: String, value, seconds: float) -> void:
 	if not is_instance_valid(target):
